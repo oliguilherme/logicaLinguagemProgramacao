89d3ca2565672d3f6110eafe916d7298435b8135
guilherme <92556199+oliguilherme@users.noreply.github.com>
2025-01-27 19:55:33 -0300

    Update main.c

--- lista-for/questao11/main.c
+++ lista-for/questao11/main.c
@@ -1,23 +1,27 @@
+
 #include <stdio.h>
 #include <locale.h>
 
 int main() {
     system("cls");
-    int entrada, i, fn_1, fn_2 = 0, fn_3;
+    int entrada, i, fn_1, fn_2, fn_3;
 
     printf("Informe um inteiro n menor que 46 e maior que zero: ");
     scanf("%d", &entrada);
 
     if (entrada < 46 && entrada > 0) {
         for (i = 1; i <= entrada; i++) {
-            if (i = 1) {
+            if (i == 1) {
+                printf("0 1");
+                fn_3 = 1;
                 fn_1 = 0;
-                fn_2 = 0;
-                fn_3 = fn_1 + fn_2;
+
             } else {
-                
+                fn_3 += fn_1;
+                fn_1 = fn_3;
+                printf(" %d", fn_1);
+            
             }
-            printf(" %d", fn_3);
 
         }
         
