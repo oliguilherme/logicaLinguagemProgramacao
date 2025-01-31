6c96eb3f1de12b9907eb5dd2a89c0b02d9ff7f61
oliguilherme <guilhermeleitesouza2018@gmail.com>
2025-01-31 10:56:20 -0300

    update

---
+++ lista-for/questao12/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  system("cls");
+
+  int x, y, aux = 0;
+
+  printf("Informe o total de números e a quantidade de colunas: ");
+  scanf("%d %d", &x, &y);
+
+  for (int i = 1; i <= x; i++) {
+    printf("%d", i);
+    printf(" ");
+    aux++;
+    if ((aux % y) == 0) {
+      printf("\n");
+    }
+  }
+}
